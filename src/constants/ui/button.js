9bb9a8beab91832f127c5259385198c24568521b
Kled <1017yu@daum.net>
2023-12-06 17:36:40 +0900

    feat: add constants

---
+++ src/constants/ui/button.js
@@ -0,0 +1,6 @@
+const BUTTON = Object.freeze({
+  kakao: '카카오 로그인',
+  google: '구글 로그인',
+});
+
+export default BUTTON;
---
+++ src/constants/ui/icon.js
@@ -0,0 +1,5 @@
+const ICON = Object.freeze({
+  buttonWidth: 24,
+});
+
+export default ICON;
