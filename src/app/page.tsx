47712b6586179a32f2ab365a78c5ac2b7c361fc0
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-12-06 13:44:14 +0900

    [CHORE] #1 프로젝트 세팅 (#2)
    * chore: next14 프로젝트 세팅
    
    * chore: set airbnb eslint
    
    * chore: set prettier
    
    * chore: set tailwind css

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <main className="">test</main>;
+}
+
+export default Home;
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+      },
+    },
+  },
+};
+export default config;
