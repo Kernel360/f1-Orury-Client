665068244961a0aa1f7e1854c9cd4866025556c5
Kled <1017yu@daum.net>
2023-12-06 17:35:48 +0900

    settings: set NextAuth kakaoProvider

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth/next';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
+  ],
+});
+
+export default handler;
