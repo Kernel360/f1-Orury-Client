4f28f7630680bd2b5a21e473e5d5b13bfceb3847
Kled <1017yu@daum.net>
2023-12-08 15:14:30 +0900

    Design: add Active Navbar icons

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -3,5 +3,21 @@ import crew from '$/crew.svg';
 import home from '$/home.svg';
 import map from '$/map.svg';
 import my from '$/my.svg';
+import community_active from '$/active/community-active.svg';
+import crew_active from '$/active/crew-active.svg';
+import home_active from '$/active/home-active.svg';
+import map_active from '$/active/map-active.svg';
+import my_active from '$/active/my-active.svg';
 
-export { community, crew, home, map, my };
+export {
+  community,
+  crew,
+  home,
+  map,
+  my,
+  community_active,
+  crew_active,
+  home_active,
+  map_active,
+  my_active,
+};
