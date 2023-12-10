0db747b4195d71b00eff3ef29272c124204ae866
Kled <1017yu@daum.net>
2023-12-11 01:41:28 +0900

    design: 헤더 하단 `Tab` 공통 컴포넌트 UI 작업

---
+++ src/app/_components/community/Tabs.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import TABS from '@/constants/community/tabs';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
+function Tabs() {
+  const pathname = usePathname();
+
+  const liClassName = (path: string) => {
+    return clsx('flex justify-center items-center border-b-4', {
+      'border-b-purple-600': pathname === path,
+      'border-b-white': pathname !== path,
+    });
+  };
+
+  return (
+    <ul className="grid grid-cols-3 h-14">
+      {Object.values(TABS).map(tab => (
+        <li key={tab.title} className={liClassName(tab.path)}>
+          {tab.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tabs;
