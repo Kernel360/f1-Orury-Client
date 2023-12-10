dcb6a936c5806e11834a7f4d0936d32f763ccfe9
Kled <1017yu@daum.net>
2023-12-10 16:43:46 +0900

    design: svg 추가 및 폴더 구조화

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,15 +1,19 @@
-import community from '$/community.svg';
-import crew from '$/crew.svg';
-import home from '$/home.svg';
-import map from '$/map.svg';
-import my from '$/my.svg';
-import community_active from '$/active/community-active.svg';
-import crew_active from '$/active/crew-active.svg';
-import home_active from '$/active/home-active.svg';
-import map_active from '$/active/map-active.svg';
-import my_active from '$/active/my-active.svg';
+import announce from '$/announce.svg';
+import bar from '$/bar.svg';
+import community from '$/navbar/community.svg';
+import crew from '$/navbar/crew.svg';
+import home from '$/navbar/home.svg';
+import map from '$/navbar/map.svg';
+import my from '$/navbar/my.svg';
+import community_active from '$/navbar/active/community-active.svg';
+import crew_active from '$/navbar/active/crew-active.svg';
+import home_active from '$/navbar/active/home-active.svg';
+import map_active from '$/navbar/active/map-active.svg';
+import my_active from '$/navbar/active/my-active.svg';
 
 export {
+  announce,
+  bar,
   community,
   crew,
   home,
