a7071e93d91081e1bc231ccf507707abeb4f078a
Kled <1017yu@daum.net>
2023-12-11 20:48:54 +0900

    feat: posts placeholder json type 추가

---
+++ src/types/community/posts.ts
@@ -0,0 +1,11 @@
+interface PostsProps {
+  id: number;
+  nickname: string;
+  title: string;
+  body: string;
+  profileImage: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export default PostsProps;
