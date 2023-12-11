8d7f5b64ce0fe9a9ccaec61cfb9599a8b13050e3
Kled <1017yu@daum.net>
2023-12-11 20:48:09 +0900

    feat: 현재 시각과 게시 시각 차이 계산 유틸 함수 추가

---
+++ src/constants/utils/getTimeDiff.ts
@@ -0,0 +1,27 @@
+export const TIME_DIFF = Object.freeze({
+  now: '방금',
+  minute: '분 전',
+  hour: '시간 전',
+  day: '일 전',
+  month: '개월 전',
+  year: '년 전',
+});
+
+export const MILLI_SECONDS = Object.freeze({
+  byMinute: 60_000,
+  byHour: 3_600_000,
+  byDay: 86_400_000,
+});
+
+export const DAYS = Object.freeze({
+  byMonth: 30,
+  byYear: 365,
+});
+
+export const TIME_THRESHOLD = Object.freeze({
+  minute: 1,
+  hour: 60,
+  day: 24,
+  month: 30,
+  year: 365,
+});
