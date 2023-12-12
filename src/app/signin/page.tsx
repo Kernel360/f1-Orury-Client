d3e3808a5bd523cce49b0a5d8ac8b1aadcebb2c3
Kled <1017yu@daum.net>
2023-12-12 21:15:20 +0900

    feat: Session type 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,12 +2,12 @@ import TEAM from '@/constants/team';
 import Kakao from '@/app/_components/ui/buttons/Kakao';
 import Title from '@/app/_components/ui/common/Title';
 
-function Page() {
+async function Page() {
   return (
     <div className="flex pt-[4rem] flex-col justify-between h-screen">
       <div className="flex flex-col max-w-[480px] mx-auto">
         <Title />
-        <span className="font-pretendard text-xl mx-auto drop-shadow-xl font-semibold">
+        <span className="mx-auto text-xl font-semibold font-pretendard drop-shadow-xl">
           {TEAM.content}
         </span>
       </div>
