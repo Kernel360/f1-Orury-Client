1e5e19daa09ee54e75dfe4cb8b40fffc49e05238
Kled <1017yu@daum.net>
2023-12-12 21:11:27 +0900

    feat: 토큰 기반 접근 제어 기능 구현

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  // 사용자가 요청하는 페이지 pathname
+  const { pathname } = req.nextUrl;
+
+  if (!token && pathname === '/my-page') {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  if (!token && pathname === '/') {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  return null;
+}
+
+// 미들웨어가 실행될 특정 pathname을 지정하면, 해당 pathname에서만 실행 가능
+export const config = {
+  mathcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+};
