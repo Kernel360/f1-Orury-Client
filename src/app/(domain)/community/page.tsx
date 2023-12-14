0a3d721084520bf224bf4f29cf32b78a0aecfafe
Kled <1017yu@daum.net>
2023-12-14 18:52:11 +0900

    feat: 서버사이드 randomUUID key 추가

--- src/app/(domain)/community/page.tsx
+++ src/app/(domain)/community/page.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Post from '@/app/_components/community/Post';
 import SearchBar from '@/app/_components/community/SearchBar';
 import Tabs from '@/app/_components/community/Tabs';
-import getPosts from './api/getPosts';
+import getPosts from '@/app/(domain)/community/api/getPosts';
+import { randomUUID } from 'crypto';
 
 async function page() {
   const posts = await getPosts();
 
   return (
-    <div>
+    <div className="bg-white pb-16">
       <SearchBar />
       <Tabs />
       <ul className="mx-4">
         {posts.map(post => (
-          <Post key={post.id} {...post} />
+          <Post key={randomUUID()} {...post} />
         ))}
       </ul>
     </div>
