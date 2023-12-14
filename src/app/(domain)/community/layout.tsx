a1f5414ef49b93e4083fbd67e6532dd8d1df5f98
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-12-14 12:15:30 +0900

    Design: Comment input bar UI 구현 (#26)
    * design: 전송(댓글 입력) 아이콘 추가
    
    * rename: 검색바 아이콘 경로 수정
    
    * style: 헤더 z-index 수정
    
    * style: 서치 바 아이콘 import 경로 수정
    
    * design: 댓글 입력 바 컴포넌트 UI 구현

--- src/app/(domain)/community/layout.tsx
+++ src/app/(domain)/community/layout.tsx
@@ -3,7 +3,7 @@ import HEADER from '@/constants/ui/common/header';
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-white">
+    <div className="bg-white h-screen">
       <Header title={HEADER.community} isBack />
       {children}
     </div>
