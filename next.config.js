e2d1519ab52cd67fbce445e7d51649dfe6c1a9ae
Kled <1017yu@daum.net>
2023-12-15 20:54:57 +0900

    chore: kakaocdn 호스트 구성

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
