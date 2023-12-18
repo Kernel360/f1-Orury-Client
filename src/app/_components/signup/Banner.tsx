848bce7c2438677081a27d9ab3e054f871192b22
Kled <1017yu@daum.net>
2023-12-18 17:45:17 +0900

    feat: 임시 배너 컴포넌트 추가

---
+++ src/app/_components/signup/Banner.tsx
@@ -0,0 +1,16 @@
+function Banner() {
+  return (
+    <div className="flex flex-col items-center h-[50vh] bg-purple-400">
+      <div className="mt-8">
+        <p className="text-white font-semibold text-center">
+          커뮤니티를 통해 암장 정보도 얻고
+        </p>
+        <p className="text-white font-semibold text-center">
+          나의 등반 메이트도 찾아요
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
