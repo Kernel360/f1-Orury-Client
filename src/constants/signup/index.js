c5bac1af2da74a6413e2f28f52f3f5ed68f2c8f9
Kled <1017yu@daum.net>
2023-12-18 17:46:26 +0900

    feat: 회원가입 관련 상수 추가

---
+++ src/constants/signup/index.js
@@ -0,0 +1,32 @@
+import { checked, unChecked } from '$/signup';
+
+const SIGNUP_BUTTON = Object.freeze({
+  content: '회원가입',
+  color: 'primary',
+});
+
+const BIRTHDAY_INPUT = Object.freeze({
+  label: '생년월일',
+  placeholder: 'YYYY-MM-DD',
+  type: 'text',
+  alt: '체크',
+  maxLength: 8,
+  checked,
+  unChecked,
+});
+
+const GENDER = Object.freeze({
+  label: '성별',
+  man: '남성',
+  woman: '여성',
+  manIdx: 1,
+  womanIdx: 2,
+});
+
+const rBirthform = /(\d{4})(\d{2})(\d{2})/;
+
+const INVALID_MESSAGE = Object.freeze({
+  birth: '⚠ 생년월일 형식이 올바르지 않습니다.',
+});
+
+export { SIGNUP_BUTTON, BIRTHDAY_INPUT, GENDER, INVALID_MESSAGE, rBirthform };
