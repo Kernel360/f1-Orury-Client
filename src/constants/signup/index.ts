bf9c3c0568958057d3adb183d02de8d256b077f2
Kled <1017yu@daum.net>
2023-12-22 10:50:32 +0900

    refactor: as const 프리징

--- src/constants/signup/index.ts
+++ src/constants/signup/index.ts
@@ -1,11 +1,11 @@
 import { checked, unChecked } from '$/signup';
 
-const SIGNUP_BUTTON = Object.freeze({
+const SIGNUP_BUTTON = {
   content: '회원가입',
   color: 'primary',
-});
+} as const;
 
-const BIRTHDAY_INPUT = Object.freeze({
+const BIRTHDAY_INPUT = {
   label: '생년월일',
   placeholder: 'YYYY-MM-DD',
   type: 'text',
@@ -13,21 +13,21 @@ const BIRTHDAY_INPUT = Object.freeze({
   maxLength: 8,
   checked,
   unChecked,
-});
+} as const;
 
-const GENDER = Object.freeze({
+const GENDER = {
   label: '성별',
   man: '남성',
   woman: '여성',
   manIdx: 1,
   womanIdx: 2,
-});
+} as const;
 
 const rBirthform = /(\d{4})(\d{2})(\d{2})/;
 
-const INVALID_MESSAGE = Object.freeze({
+const INVALID_MESSAGE = {
   birth: '⚠ 생년월일 형식이 올바르지 않습니다.',
   gender: '⚠ 성별을 선택해주세요.',
-});
+} as const;
 
 export { SIGNUP_BUTTON, BIRTHDAY_INPUT, GENDER, INVALID_MESSAGE, rBirthform };
