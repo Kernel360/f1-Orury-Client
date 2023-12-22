f78ae7bf52e440e4b1ae75bc3a3360a78a09d58a
Kled <1017yu@daum.net>
2023-12-22 17:38:57 +0900

    fix: 모달 상수 파일 병합

---
+++ src/constants/ui/common/modal.ts
@@ -0,0 +1,14 @@
+import { EOL } from 'os';
+
+export const MODAL = {
+  withdrawal: {
+    title: '회원 탈퇴',
+    content: `회원 탈퇴를 진행하시겠습니까? ${EOL}한번 삭제된 정보는 복구할 수 없습니다.`,
+    okContent: '탈퇴',
+  },
+  deleteComment: {
+    title: '댓글 삭제',
+    content: `댓글을 삭제하시겠습니까?? ${EOL}삭제된 내용은 복구할 수 없습니다.`,
+    okContent: '삭제',
+  },
+} as const;
