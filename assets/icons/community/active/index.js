d4201a55fcd70d76eecac60fb5926d95ae430053
Kled <1017yu@daum.net>
2023-12-22 17:40:47 +0900

    design: 아이콘 변경 및 액티브 아이콘 추가

--- assets/icons/community/active/index.js
+++ assets/icons/community/active/index.js
@@ -1,3 +1,4 @@
-import thumbsUp_active from '$/community/active/thumbs-up-active.svg';
+import like_active_red from '$/community/active/like-active-red.svg';
+import like_active_grey from '$/community/active/like-active-grey.svg';
 
-export { thumbsUp_active };
+export { like_active_red, like_active_grey };
--- assets/icons/community/index.js
+++ assets/icons/community/index.js
@@ -1,7 +1,7 @@
 import comment from '$/community/comment.svg';
-import thumbsUp from '$/community/thumbs-up.svg';
+import like_outline from '$/community/like-outline.svg';
 import view from '$/community/view.svg';
 import reply from '$/community/reply.svg';
 import write from '$/community/write.svg';
 
-export { comment, thumbsUp, view, reply, write };
+export { comment, like_outline, view, reply, write };
