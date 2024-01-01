adfb6c67fe98255a572f923a976f537adb440b80
Kled <1017yu@daum.net>
2024-01-01 16:42:54 +0900

    style: css 세부 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Title from '@/app/_components/common/Title';
 
 async function Page() {
   return (
-    <div className="flex pt-[4rem] flex-col justify-between h-screen">
+    <div className="flex pt-[8rem] flex-col justify-between h-screen bg-white">
       <div className="flex flex-col max-w-[480px] mx-auto">
         <Title />
         <span className="mx-auto text-xl font-semibold font-pretendard drop-shadow-xl">
           {TEAM.content}
         </span>
       </div>
-      <div className="flex justify-center pb-[2rem] mx-4">
+      <div className="flex justify-center pb-[8rem] mx-4">
         <Kakao />
       </div>
     </div>
