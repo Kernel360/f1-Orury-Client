81f5f9cf3a35414b7959c8385f678adb1347870e
Jihyeong00 <iolpd42@gmail.com>
2024-01-08 10:29:58 +0900

    feat: chip의 키 값을 추가한다.

--- src/app/service/map/_components/Review/IconChipList.tsx
+++ src/app/service/map/_components/Review/IconChipList.tsx
@@ -1,6 +1,7 @@
 import { IconChipListProps } from '@/types/map/ReviewProps';
 import IconContainer from '@/app/service/map/_components/Review/IconContainer';
 import { Chip, Stack } from '@mui/material';
+import { v4 } from 'uuid';
 
 function IconChipList({ item }: IconChipListProps) {
   return (
@@ -9,6 +10,7 @@ function IconChipList({ item }: IconChipListProps) {
         const { count, level } = value;
         return (
           <Chip
+            key={v4()}
             className="shadow p-1"
             avatar={<IconContainer value={level} />}
             variant="outlined"
