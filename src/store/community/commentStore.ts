c463fd6af78d169d29a50fa81cebd34596b6cb11
Kled <1017yu@daum.net>
2024-01-15 23:34:24 +0900

    feat: 커뮤니티 관련 zustand store 메서드 추가

--- src/store/community/commentStore.ts
+++ src/store/community/commentStore.ts
@@ -3,9 +3,17 @@ import { CommentStateProps } from '@/types/community/comment';
 
 const useCommentStore = create<CommentStateProps>(set => ({
   commentId: 0,
+  parentId: 0,
   triggerModify: false,
+  isFocus: false,
+  isEditMode: false,
+  isReplyMode: false,
   setCommentId: value => set({ commentId: value }),
+  setParentId: value => set({ parentId: value }),
   setTriggerModify: value => set({ triggerModify: value }),
+  setIsFocus: value => set({ isFocus: value }),
+  setIsEditMode: value => set({ isEditMode: value }),
+  setIsReplyMode: value => set({ isReplyMode: value }),
 }));
 
 export default useCommentStore;
--- src/store/community/postsStore.ts
+++ src/store/community/postsStore.ts
@@ -1,9 +1,14 @@
 import { create } from 'zustand';
-import { PostsStateProps } from '@/types/community/posts';
+import { PostsStateProps, OnePostProps } from '@/types/community/post';
 
-const usePostsState = create<PostsStateProps>(set => ({
+export const usePostsState = create<PostsStateProps>(set => ({
   categoryId: 1,
   setCategoryId: id => set({ categoryId: id }),
 }));
 
-export default usePostsState;
+export const useOnePostState = create<OnePostProps>(set => ({
+  title: '',
+  content: '',
+  setTitle: value => set({ title: value }),
+  setContent: value => set({ content: value }),
+}));
