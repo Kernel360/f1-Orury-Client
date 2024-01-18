b377d2a65ac2d43e66d600bc8b2d82cbab030f26
Kled <1017yu@daum.net>
2024-01-18 13:14:23 +0900

    fix: 미사용 세션 컨텍스트 제거 후 쿠키 프로바이더 세팅

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { ReactNode } from 'react';
-import mockEnable from '@/lib/msw/mock-enable';
-import { SessionContext } from '@/app/api/auth/SessionContext';
+// import mockEnable from '@/lib/msw/mock-enable';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { CookiesProvider } from 'react-cookie';
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enable') {
-  mockEnable();
-}
+// if (process.env.NEXT_PUBLIC_API_MOCKING === 'enable') {
+//   mockEnable();
+// }
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <SessionContext>{children}</SessionContext>
+      <CookiesProvider>{children}</CookiesProvider>
     </AppRouterCacheProvider>
   );
 }
