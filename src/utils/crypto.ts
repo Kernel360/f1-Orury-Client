3731f271d5c9d8d4f5b54fdc01a449884bd58dea
Kled <1017yu@daum.net>
2024-01-19 10:56:20 +0900

    fix: salting undefined 오류 수정

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -11,14 +11,17 @@ export const encrypt = (payload: string) => {
   return encrypted;
 };
 
-export const decrypt = (ciphertext: string) => {
+export const decrypt = (ciphertext?: string) => {
+  let decrypted_bytes;
   if (!secretKey) return null;
 
   // secretKey를 기반으로 복호화
-  const decrypted_bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+  if (ciphertext) {
+    decrypted_bytes = CryptoJS.AES.decrypt(ciphertext, secretKey as string);
+  }
 
   // 얻은 복호화된 바이트를 UTF-8 형식의 문자열로 디코딩
-  const decrypted = decrypted_bytes.toString(CryptoJS.enc.Utf8);
+  const decrypted = decrypted_bytes?.toString(CryptoJS.enc.Utf8);
 
   return decrypted;
 };
