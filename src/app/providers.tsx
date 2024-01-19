e00dfcc04acca1fabbc2eb23484a55dae59636a6
Kled <1017yu@daum.net>
2024-01-19 15:30:30 +0900

    fix: provider 불필요 주석 제거

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { ReactNode } from 'react';
-// import mockEnable from '@/lib/msw/mock-enable';
+import mockEnable from '@/lib/msw/mock-enable';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { CookiesProvider } from 'react-cookie';
 
-// if (process.env.NEXT_PUBLIC_API_MOCKING === 'enable') {
-//   mockEnable();
-// }
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enable') {
+  mockEnable();
+}
 
 function Providers({ children }: { children: ReactNode }) {
   return (
