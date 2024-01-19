2822d8ad114032c9913c51276226a8a3528cd7d5
Hyungjoon Cho <selcon1996@gmail.com>
2024-01-19 14:23:28 +0900

    fix : 자동 배포 로직 수정

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,8 @@ sudo npm install
 if [ $? -eq 0 ]; then
   # 실행 중인 경우
   echo "til-dev 프로세스가 실행 중입니다."
-  sudo npm run pm2:reload:dev
 else
   # 실행 중이 아닌 경우
   echo "til-dev 프로세스가 실행되지 않았습니다."
-  sudo npm run pm2:start:dev
+  sudo npm run dev
 fi
