aa029a3901416afc9c8b80fefc3551ff9af4d50e
Kled <1017yu@daum.net>
2024-01-19 15:33:02 +0900

    fix: Partial<T>으로 타입 수정

--- src/types/common/cookie.ts
+++ src/types/common/cookie.ts
@@ -1,25 +1,25 @@
 interface CookieSetOptions {
-  path?: string;
-  expires?: Date;
-  maxAge?: number;
-  domain?: string;
-  secure?: boolean;
-  httpOnly?: boolean;
-  sameSite?: boolean | 'none' | 'lax' | 'strict';
+  path: string;
+  expires: Date;
+  maxAge: number;
+  domain: string;
+  secure: boolean;
+  httpOnly: boolean;
+  sameSite: boolean | 'none' | 'lax' | 'strict';
 }
 
 interface CookieGetOptions {
-  doNotParse?: boolean;
-  doNotUpdate?: boolean;
+  doNotParse: boolean;
+  doNotUpdate: boolean;
 }
 
 export interface SetCookieProps {
   name: string;
   value: any;
-  options?: CookieSetOptions | undefined;
+  options?: Partial<CookieSetOptions>;
 }
 
 export interface GetCookieProps {
   name: string;
-  options?: CookieGetOptions | undefined;
+  options?: Partial<CookieGetOptions>;
 }
