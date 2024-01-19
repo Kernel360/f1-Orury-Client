e9a1710e4a401f98d1b9d2547eb5b57c708806b0
Joon <44130863+kkkapuq@users.noreply.github.com>
2024-01-19 14:13:46 +0900

    feat : 클라이언트 ec2 자동배포 테스트 (#62)

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/deploy
+REPOSITORY_PROD=/home/ubuntu/deploy
+
+
+echo "개발 서버 배포"
+cd "${REPOSITORY}"
+sudo npm install
+if [ $? -eq 0 ]; then
+  # 실행 중인 경우
+  echo "til-dev 프로세스가 실행 중입니다."
+  sudo npm run pm2:reload:dev
+else
+  # 실행 중이 아닌 경우
+  echo "til-dev 프로세스가 실행되지 않았습니다."
+  sudo npm run pm2:start:dev
+fi
