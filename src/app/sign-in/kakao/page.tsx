7b4b06e48f897c81a3448a5e16bfd6206798bf77
Kled <1017yu@daum.net>
2024-01-24 17:42:23 +0900

    feat: 로그인 status code 따른 분기처리

--- src/app/sign-in/kakao/page.tsx
+++ src/app/sign-in/kakao/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
 import { setCookie } from '@/lib/cookie';
 import { encrypt } from '@/utils/crypto';
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useToast } from '@/app/_components/ui/use-toast';
+import { ERROR_CODE, STATUS_CODE } from '@/constants/api/statusCode';
 
+import CALLBACK_URL from '@/constants/url';
 import getUserInfo from '@/app/api/auth/getUserInfo';
 import useSignInState from '@/store/sign-in/signInStore';
 
 // 카카오 소셜 로그인 REDIRECT URI PAGE
 function Page() {
+  const { home, service } = CALLBACK_URL;
+  const { invalidEmail, noAccount, haveAnotherAccount } = ERROR_CODE;
+  const { toast } = useToast();
   const router = useRouter();
   const { signUpType } = useSignInState();
 
@@ -32,13 +38,45 @@ function Page() {
         });
       }
 
-      // TODO: STATUS CODE 상수화 필요
-      if (response?.status === 200) router.push('/service');
-      if (response?.status === 404) router.push('/sign-up');
+      // 로그인 성공
+      if (response?.status === STATUS_CODE.ok) router.push(service);
+
+      // 올바르지 않은 형식의 이메일을 가진 유저일 경우
+      if (response?.status === invalidEmail) {
+        router.push(home);
+        toast({
+          description: response.message,
+        });
+      }
+
+      // 가입된 계정이 존재하지 않은 비회원인 경우
+      if (response?.status === noAccount) {
+        router.push(service);
+        toast({
+          description: response.message,
+        });
+      }
+
+      // 다른 소셜 마디어를 통해 가입된 이력이 있는 경우
+      if (response?.status === haveAnotherAccount) {
+        router.push(home);
+        toast({
+          description: response.message,
+        });
+      }
     };
 
     signIn();
-  }, [router, signUpType]);
+  }, [
+    haveAnotherAccount,
+    home,
+    invalidEmail,
+    noAccount,
+    router,
+    service,
+    signUpType,
+    toast,
+  ]);
 
   return <div />;
 }
