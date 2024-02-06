07f37f11cb1641d5dcc7e2843ea56e1417a0609a
Kled <1017yu@daum.net>
2024-02-06 10:19:05 +0900

    fix: manifest 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
         name="viewport"
         content="viewport-fit=cover, width=device-width, initial-scale=1.0"
       />
-      <link rel="manifest" href="/manifest.json" />
+      <link rel="/manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/favicons/apple-icon.png" />
 
       <link
