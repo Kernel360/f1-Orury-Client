33f11dbf905122cde15e5615cbe10961e3d9f18f
Kled <1017yu@daum.net>
2024-02-07 11:14:40 +0900

    fix: 미사용 이미지 주소 삭제

--- next.config.js
+++ next.config.js
@@ -4,18 +4,13 @@ const withPWA = require('next-pwa');
 
 const nextConfig = {
   reactStrictMode: false,
+
   transpilePackages: ['crypto-js'],
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'via.placeholder.com',
         port: '',
         pathname: '/**',
       },
