4c841e65864eb9e2695a94d8d202b4886eeb3ee2
Kled <1017yu@daum.net>
2024-02-23 15:00:05 +0900

    refactor: 카테고리 선택 디자인 변경 및 디폴트 추가

--- src/app/_components/common/Form.tsx
+++ src/app/_components/common/Form.tsx
@@ -1,25 +1,24 @@
 'use client';
 
+import autoResize from '@/utils/autoResize';
 import TextInput from '@/app/_components/common/TextInput';
-import Content from '@/app/_components/common/Content';
 import post from '@/app/service/community/api/post';
 import PhotoBooth from '@/app/service/community/_components/PhotoBooth';
 import editPost from '@/app/service/community/api/editPost';
-import usePostListInfinite from '@/hooks/community/usePostListInfinite';
-import * as D from '@/app/_components/ui/dropdown-menu';
+import * as R from '@/app/_components/ui/radio-group';
 import * as F from '@/app/_components/ui/form';
+import usePostListInfinite from '@/hooks/community/get/infinite/usePostListInfinite';
 
 import { useState } from 'react';
 import { getFormData } from '@/utils/getFormData';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { ChevronsDown } from 'lucide-react';
 import { useForm } from 'react-hook-form';
-import { cn } from '@/lib/utils';
 import { Button } from '@/app/_components/ui/button';
-import { CATEGORIES } from '@/constants/common/form';
 import { useOnePostState, usePostsState } from '@/store/community/postsStore';
 import { FormSchema, FormSchemaType } from '@/app/schema';
 import { useToast } from '@/app/_components/ui/use-toast';
+import { useDebouncedCallback } from 'use-debounce';
+import { Textarea } from '@/app/_components/ui/textarea';
 import type { FormType } from '@/types/common/form';
 
 function Form({ ...props }: FormType) {
@@ -29,9 +28,9 @@ function Form({ ...props }: FormType) {
     content,
     isPost,
     isPostDetail,
-    isReview,
     setIsSheetOpen,
     editHandler,
+    category,
   } = props;
   const { toast } = useToast();
   const { categoryId, setCategoryId } = usePostsState();
@@ -40,10 +39,15 @@ function Form({ ...props }: FormType) {
   const [images, setImages] = useState<File[]>([]);
 
   const form = useForm<FormSchemaType>({
+    defaultValues: {
+      category,
+      title,
+      content,
+    },
     resolver: zodResolver(FormSchema),
   });
 
-  const onSubmit = async (data: FormSchemaType) => {
+  const onSubmit = useDebouncedCallback(async (data: FormSchemaType) => {
     if (isPost && setIsSheetOpen) {
       const formData = getFormData({ jsonData: JSON.stringify(data), images });
       const message = await post(formData);
@@ -51,7 +55,7 @@ function Form({ ...props }: FormType) {
       await mutate();
       setIsSheetOpen(false);
 
-      toast({ variant: 'success', description: message });
+      toast({ variant: 'success', description: message, duration: 2000 });
     }
 
     if (isPostDetail && postId && editHandler) {
@@ -60,17 +64,15 @@ function Form({ ...props }: FormType) {
         images,
       });
 
-      await editPost(formData);
+      const message = await editPost(formData);
       await mutate();
       editHandler();
       setTitle(data.title);
       setContent(data.content);
-    }
 
-    if (isReview) {
-      // API CALL
+      toast({ variant: 'success', description: message, duration: 2000 });
     }
-  };
+  }, 300);
 
   return (
     <F.Form {...form}>
@@ -83,46 +85,31 @@ function Form({ ...props }: FormType) {
             control={form.control}
             name="category"
             render={({ field }) => (
-              <F.FormItem className="w-full flex flex-col">
-                <D.DropdownMenu>
-                  <D.DropdownMenuTrigger asChild>
-                    <F.FormControl>
-                      <Button
-                        variant="underline"
-                        role="combobox"
-                        className={cn(
-                          'w-full justify-between',
-                          !field.value && 'text-muted-foreground',
-                        )}
-                      >
-                        {field.value
-                          ? CATEGORIES.find(
-                              categoryValue =>
-                                categoryValue.value === field.value,
-                            )?.label
-                          : '카테고리 선택'}
-                        <ChevronsDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                      </Button>
-                    </F.FormControl>
-                  </D.DropdownMenuTrigger>
-
-                  <D.DropdownMenuContent className="w-screen max-w-[734px] p-0">
-                    {CATEGORIES.map(category => (
-                      <D.DropdownMenuItem
-                        {...field}
-                        className="bg-white justify-center"
-                        textValue={category.label}
-                        onSelect={() => {
-                          form.setValue('category', category.value);
-                          setCategoryId(category.value);
-                        }}
-                      >
-                        {category.label}
-                      </D.DropdownMenuItem>
-                    ))}
-                  </D.DropdownMenuContent>
-                </D.DropdownMenu>
-                <F.FormMessage className="text-warning pl-2 text-sm" />
+              <F.FormItem className="space-y-3">
+                <F.FormMessage className="text-warning text-xs" />
+                <F.FormControl>
+                  <R.RadioGroup
+                    onValueChange={value => {
+                      field.onChange(Number(value));
+                      setCategoryId(Number(value));
+                    }}
+                    defaultValue={category?.toString()}
+                    className="flex space-x-2"
+                  >
+                    <F.FormItem className="flex items-center space-x-3 space-y-0">
+                      <F.FormControl>
+                        <R.RadioGroupItem value="1" />
+                      </F.FormControl>
+                      <F.FormLabel className="text-md">자유게시판</F.FormLabel>
+                    </F.FormItem>
+                    <F.FormItem className="flex items-center space-x-3 space-y-0">
+                      <F.FormControl>
+                        <R.RadioGroupItem value="2" />
+                      </F.FormControl>
+                      <F.FormLabel className="text-md">QnA</F.FormLabel>
+                    </F.FormItem>
+                  </R.RadioGroup>
+                </F.FormControl>
               </F.FormItem>
             )}
           />
@@ -133,6 +120,8 @@ function Form({ ...props }: FormType) {
             name="title"
             render={({ field }) => (
               <F.FormItem className="w-full flex flex-col">
+                <F.FormMessage className="text-warning text-xs" />
+
                 <F.FormControl>
                   <TextInput
                     placeholder="제목"
@@ -142,7 +131,6 @@ function Form({ ...props }: FormType) {
                     {...field}
                   />
                 </F.FormControl>
-                <F.FormMessage className="text-warning pl-2 text-xs" />
               </F.FormItem>
             )}
           />
@@ -153,10 +141,18 @@ function Form({ ...props }: FormType) {
             name="content"
             render={({ field }) => (
               <F.FormItem className="w-full flex flex-col">
+                <F.FormMessage className="text-warning text-xs" />
                 <F.FormControl>
-                  <Content placeholder="글 내용" {...field} content={content} />
+                  <Textarea
+                    placeholder="글 내용"
+                    className="resize-none border-none max-h-96"
+                    defaultValue={content}
+                    onKeyUp={e => autoResize(e.target as HTMLTextAreaElement)}
+                    onKeyDown={e => autoResize(e.target as HTMLTextAreaElement)}
+                    rows={10}
+                    {...field}
+                  />
                 </F.FormControl>
-                <F.FormMessage className="text-warning pl-2 text-xs" />
               </F.FormItem>
             )}
           />
