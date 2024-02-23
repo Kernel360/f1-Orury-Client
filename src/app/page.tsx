a6dabe03b5d3f6484843e7e1a11ef94cb4c68268
Kled <1017yu@daum.net>
2024-02-23 14:59:32 +0900

    fix: 중복 로직 제거 (미들웨어 동작)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,11 @@ import Kakao from '@/app/_components/buttons/Kakao';
 import Title from '@/app/_components/common/Title';
 import Content from '@/app/_components/home/Content';
 import Button from '@/app/_components/buttons/Button';
-import CALLBACK_URL from '@/constants/url';
 import GuideModal from '@/app/_components/home/GuideModal';
 
-import { useEffect, useState } from 'react';
-import { getCookie } from '@/lib/cookie';
-import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 function Page() {
-  const router = useRouter();
   const [isClicked, setIsClicked] = useState(false);
 
   const handleButtonClick = () => {
@@ -22,12 +18,6 @@ function Page() {
   const handleCloseModalClick = () => {
     setIsClicked(false);
   };
-
-  useEffect(() => {
-    if (getCookie({ name: 'access_token' })) {
-      router.push(CALLBACK_URL.service);
-    }
-  }, []);
 
   return (
     <div className="flex pt-24 flex-col justify-between h-screen bg-[url('../../assets/images/background.webp')] bg-cover bg-opacity-90">
