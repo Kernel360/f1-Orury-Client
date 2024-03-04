86bcf2052f8ae44cccbf4f59ae8d8ae08cb284a0
Kled <1017yu@daum.net>
2024-03-05 01:25:13 +0900

    chore: datepicker, dayjs 구성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
   ],
   theme: {
     container: {
