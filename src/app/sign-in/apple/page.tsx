4feed1bc55df0cb542def651c5642d042789426f
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2024-03-06 18:48:02 +0900

    Fix: 토큰 encrypt 에러 해결

--- src/app/sign-in/apple/page.tsx
+++ src/app/sign-in/apple/page.tsx
@@ -8,13 +8,13 @@ import Lottie from 'react-lottie';
 import lottieOption from '@/utils/lottieOption';
 
 import { useEffect } from 'react';
-import { setCookie } from '@/lib/cookie';
 import { encrypt } from '@/utils/crypto';
 import { useRouter } from 'next/navigation';
 import { getEmail } from '@/utils/getEmail';
 import { useToast } from '@/app/_components/ui/use-toast';
 import { setTokensInCookies } from '@/utils/setTokensInCookies';
 import { ERROR_CODE, STATUS_CODE } from '@/constants/api/statusCode';
+import { SIGN_UP_ERROR_MESSAGES } from '@/constants/sign-in';
 
 // APPLE 소셜 로그인 REDIRECT URI PAGE
 function Page() {
@@ -27,6 +27,8 @@ function Page() {
   useEffect(() => {
     const signIn = async () => {
       let email;
+      let authToken;
+
       const code = new URL(window.location.href).searchParams.get('code');
       const response = await getUserInfo({ code, signUpType });
 
@@ -48,15 +50,18 @@ function Page() {
           break;
 
         case noAccount:
-          email = getEmail(response?.data.access_token);
+          authToken = encrypt(response.data.access_token);
+          email = getEmail(authToken);
 
-          setCookie({
-            name: 'access_token',
-            value: encrypt(response.data.access_token),
-            options: { path: '/' },
-          });
+          if (authToken) {
+            sessionStorage.setItem('auth_token', authToken);
+          } else {
+            response.message = SIGN_UP_ERROR_MESSAGES.noAuthToken;
 
-          if (email) setEmail(email as string);
+            router.push(home);
+          }
+
+          if (typeof email === 'string') setEmail(email);
 
           router.push(signUp);
           break;
@@ -70,7 +75,11 @@ function Page() {
           break;
       }
 
-      toast({ variant: 'default', description: response?.message });
+      toast({
+        variant: 'default',
+        description: response?.message || SIGN_UP_ERROR_MESSAGES.default,
+        duration: 2000,
+      });
     };
 
     signIn();
