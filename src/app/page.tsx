5d44cc8f7a0fcd9eef266edbdafc99a17e4747e4
Kled <1017yu@daum.net>
2024-03-08 10:52:00 +0900

    fix: 쿠키 여부에 따른 자동로그인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 'use client';
 
+import CALLBACK_URL from '@/constants/url';
 import Kakao from '@/app/_components/buttons/Kakao';
 import Title from '@/app/_components/common/Title';
 import Content from '@/app/_components/home/Content';
 import Button from '@/app/_components/buttons/Button';
 import GuideModal from '@/app/_components/home/GuideModal';
+import AppleSignIn from '@/app/_components/buttons/Apple';
 
 import { useEffect, useState } from 'react';
-import { getCookie } from '@/lib/cookie';
 import { useRouter } from 'next/navigation';
-import AppleSignIn from './_components/buttons/Apple';
+import { getCookie } from '@/lib/cookie';
 
 function Page() {
+  const router = useRouter();
   const [isClicked, setIsClicked] = useState(false);
 
   const handleButtonClick = () => {
@@ -21,6 +23,12 @@ function Page() {
   const handleCloseModalClick = () => {
     setIsClicked(false);
   };
+
+  useEffect(() => {
+    if (getCookie({ name: 'access_token' })) {
+      router.push(CALLBACK_URL.service);
+    }
+  }, []);
 
   return (
     <div className="flex pt-24 flex-col justify-between h-screen bg-[url('../../assets/images/background.webp')] bg-cover bg-opacity-90">
