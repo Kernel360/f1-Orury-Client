f5d7a982e6c0454bf6485d30b96e4414e7f7e312
Kled <1017yu@daum.net>
2024-03-12 10:10:27 +0900

    refactor: 고객의 소리함 제거

--- src/app/service/_components/Notice.tsx
+++ src/app/service/_components/Notice.tsx
@@ -4,12 +4,11 @@ import Autoplay from 'embla-carousel-autoplay';
 import * as C from '@/app/_components/ui/carousel';
 import useEmblaCarousel from 'embla-carousel-react';
 
-// import { v4 } from 'uuid';
+import { v4 } from 'uuid';
 import { COLOR } from '@/styles/color';
 import { Megaphone, Tally1 } from 'lucide-react';
 import { EmblaOptionsType } from 'embla-carousel';
 import { useEffect } from 'react';
-import Link from 'next/link';
 
 function Notice() {
   const OPTIONS: EmblaOptionsType = { align: 'start', loop: true, axis: 'y' };
@@ -36,16 +35,11 @@ function Notice() {
 
       <C.Carousel opts={OPTIONS} orientation="vertical">
         <C.CarouselContent ref={emblaRef}>
-          {/* {NOTICE.map(value => (
+          {NOTICE.map(value => (
             <C.CarouselItem key={v4()}>
               <span className="ellipsis">{value}</span>
             </C.CarouselItem>
-          ))} */}
-          <C.CarouselItem>
-            <Link href="https://forms.gle/eLejpGHoE1PTyn79A" target="_blank">
-              Orury 고객의 소리함 바로가기
-            </Link>
-          </C.CarouselItem>
+          ))}
         </C.CarouselContent>
       </C.Carousel>
     </div>
