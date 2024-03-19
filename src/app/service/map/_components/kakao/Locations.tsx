9652a958171b2569117f329c7e8b7da40f6b6fdc
ImHyunJeong <fltxld3@naver.com>
2024-03-19 15:20:33 +0900

    feat: 현재위치 불러오는 함수 추가

---
+++ src/app/service/map/_components/kakao/Locations.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable lines-around-directive */
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function Location() {
+  const [location, setLocation] = useState<
+    { latitude: number; longitude: number } | string
+  >('');
+
+  function success(position: GeolocationPosition) {
+    setLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  function handleError() {
+    setLocation({
+      latitude: 37.483034,
+      longitude: 126.902435,
+    });
+    console.log('Failed to retrieve location');
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, handleError);
+    } else {
+      handleError(); // geolocation이 없을때 에러처리
+    }
+  }, []);
+
+  return location;
+}
