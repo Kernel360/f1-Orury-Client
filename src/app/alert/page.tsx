530042f09bb4dc9d31f4d47d4626e65c7458c746
Kled <1017yu@daum.net>
2024-03-25 22:53:23 +0900

    feat: key 추가

--- src/app/alert/page.tsx
+++ src/app/alert/page.tsx
@@ -1,6 +1,8 @@
-import { alertMock } from '@/__mock__/data/alert.mock';
-import Header from '@/app/_components/common/Header';
 import Card from '@/app/alert/_components/Card';
+import Header from '@/app/_components/common/Header';
+
+import { v4 } from 'uuid';
+import { alertMock } from '@/__mock__/data/alert.mock';
 
 function page() {
   return (
@@ -8,7 +10,7 @@ function page() {
       <Header title="알림" isBack />
       <div className="bg-grey-50 h-[calc(100vh-48px)] pt-2">
         {Object.values(alertMock).map(value => (
-          <Card {...value} />
+          <Card key={v4()} {...value} />
         ))}
       </div>
     </section>
