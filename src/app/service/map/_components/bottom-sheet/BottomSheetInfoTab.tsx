4ae4b623bbd9b107c182876bda061c27b6fa0a62
ImHyunJeong <fltxld3@naver.com>
2024-03-28 17:17:46 +0900

    feat: BottomSheet Tab추가, 컴포넌트 분리

---
+++ src/app/service/map/_components/bottom-sheet/BottomSheetInfoTab.tsx
@@ -0,0 +1,104 @@
+import { BottomSheetInnerProps } from '@/types/map/BottomSheetProps';
+import { ChevronDown, Copy } from 'lucide-react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+export default function BottomSheetInfoTab({ data }: BottomSheetInnerProps) {
+  const {
+    address,
+    business_hours,
+    id,
+    instagram_link,
+    kakao_map_link,
+    road_address,
+    score_average,
+    setting_day,
+    bar_chart_data,
+    line_chart_data,
+    doing_business,
+    images,
+    name,
+    phone_number,
+    position,
+  } = data;
+  // 현재 요일 조회 (암장 영업일을 현재기준으로 상단에 렌더링하기때문)
+  const week = ['일', '월', '화', '수', '목', '금', '토', '일'];
+  const curDay = week[new Date().getDay()];
+
+  // business_hours에 현재 요일과 일치한것 filter 후 return
+  const filteredBusinessHours = business_hours.filter(business_hour => {
+    return Object.keys(business_hour).includes(curDay);
+  });
+  // 주소 복사
+  const copyAdd = () => {
+    window.navigator.clipboard.writeText(road_address).then(() => {
+      // 복사가 완료되면 호출된다.
+      alert('복사완료');
+    });
+  };
+
+  return (
+    <div>
+      <main className="h-fit" style={{ border: '2px solid red' }}>
+        <section className=" gap-[0.75rem] my-5">
+          <div className="flex">
+            <span className="rounded px-1 gap-8px bg-[#B79DFF] text-[#ffff] mr-1">
+              {doing_business ? '영업중' : '영업 종료'}
+            </span>
+            <h2 className="text-gray-900 font-semibold text-base">운영 시간</h2>
+          </div>
+
+          <div className="my-2">
+            <div>{`[${Object.keys(filteredBusinessHours[0])}] ${Object.values(filteredBusinessHours[0])}`}</div>
+            <ul>
+              <details className="cursor-pointer">
+                <summary className="list-none">
+                  <ChevronDown size={14} />
+                </summary>
+                {business_hours.map(business_hour =>
+                  Object.entries(business_hour).map(([day, time]) => (
+                    <div key={day}>{`[${day}] ${time}`}</div>
+                  )),
+                )}
+                {setting_day && (
+                  <div className="text-[0.875rem]">
+                    {`[Setting Day] ${setting_day}`}
+                  </div>
+                )}
+              </details>
+            </ul>
+          </div>
+        </section>
+        <div className="shadow-custom-line h-[1px] py-1" />
+        <section className="my-5">
+          <h2 className="text-gray-900 font-semibold text-base">센터 정보</h2>
+        </section>
+        <div className="shadow-custom-line h-[1px] py-1" />
+        <section className="my-5">
+          <h2 className="text-gray-900 font-semibold text-base">위치</h2>
+          <Map
+            center={{ lat: position.latitude, lng: position.longitude }}
+            style={{
+              width: '90%',
+              height: '120px',
+              borderRadius: '8px',
+              margin: 'auto',
+            }}
+          >
+            <MapMarker
+              position={{ lat: position.latitude, lng: position.longitude }}
+            />
+          </Map>
+          <div>{road_address}</div>
+          <button
+            type="button"
+            onClick={copyAdd}
+            className="flex justify-center align-center w-[100px] h-[37px] px-6px pt-16px pb-12px gap-4px rounded-lg border border-solid border-[#E5E7EB]"
+          >
+            <Copy size={14} />
+            주소복사
+          </button>
+        </section>
+      </main>
+    </div>
+  );
+}
--- src/app/service/map/_components/bottom-sheet/BottomSheetInner.tsx
+++ src/app/service/map/_components/bottom-sheet/BottomSheetInner.tsx
@@ -10,8 +10,42 @@ import { aBeeZee } from '@/styles/fonts';
 import MapCarousel from '@/app/_components/review/review-component/MapCarousel';
 import useReviewStore from '@/store/review/reviewStore';
 import { cn } from '@/lib/utils';
-import getGymState from '@/utils/getGymState';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Tab, Tabs, Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import BottomSheetInfoTab from './BottomSheetInfoTab';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 /**
  * @description 바텀시트의 내부 콘테이너로서 내용물을 보여주는데 초점을 두고 있습니다.
@@ -37,25 +71,14 @@ function BottomSheetInner({ data }: BottomSheetInnerProps) {
     position,
   } = data;
 
-  // 현재 요일 조회 (암장 영업일을 현재기준으로 상단에 렌더링하기때문)
-  const week = ['일', '월', '화', '수', '목', '금', '토', '일'];
-  const curDay = week[new Date().getDay()];
+  const [tabValue, setTabValue] = useState(0);
 
-  // business_hours에 현재 요일과 일치한것 filter 후 return
-  const filteredBusinessHours = business_hours.filter(business_hour => {
-    return Object.keys(business_hour).includes(curDay);
-  });
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
   const onModalOpen = () => {
     onReview(id);
-  };
-
-  // 주소 복사
-  const copyAdd = () => {
-    window.navigator.clipboard.writeText(road_address).then(() => {
-      // 복사가 완료되면 호출된다.
-      alert('복사완료');
-    });
   };
 
   return (
@@ -68,73 +91,33 @@ function BottomSheetInner({ data }: BottomSheetInnerProps) {
         sizes="100vw"
         className="w-screen h-64"
       />
+      {/*
       <div className="flex justify-end p-[0.75rem]">
         <button type="button" onClick={onModalOpen}>
           리뷰탭 만들어야하지롱 컴포넌트 분리해야하밍
         </button>
       </div>
-
-      <main className="p-[1rem] h-fit" style={{ border: '2px solid red' }}>
-        <section className=" gap-[0.75rem] my-5">
-          <div className="flex">
-            <span className="rounded px-1 gap-8px bg-[#B79DFF] text-[#ffff] mr-1">
-              {doing_business ? '영업중' : '영업 종료'}
-            </span>
-            <h2 className="text-gray-900 font-semibold text-base">운영 시간</h2>
-          </div>
-
-          <div className="my-2">
-            <div>{`[${Object.keys(filteredBusinessHours[0])}] ${Object.values(filteredBusinessHours[0])}`}</div>
-            <ul>
-              <details className="cursor-pointer">
-                <summary className="list-none">
-                  <ChevronDown size={14} />
-                </summary>
-                {business_hours.map(business_hour =>
-                  Object.entries(business_hour).map(([day, time]) => (
-                    <div key={day}>{`[${day}] ${time}`}</div>
-                  )),
-                )}
-                {setting_day && (
-                  <div className="text-[0.875rem]">
-                    {`[Setting Day] ${setting_day}`}
-                  </div>
-                )}
-              </details>
-            </ul>
-          </div>
-        </section>
-        <div className="shadow-custom-line h-[1px] py-1" />
-        <section className="my-5">
-          <h2 className="text-gray-900 font-semibold text-base">센터 정보</h2>
-        </section>
-        <div className="shadow-custom-line h-[1px] py-1" />
-        <section className="my-5">
-          <h2 className="text-gray-900 font-semibold text-base">위치</h2>
-          <Map
-            center={{ lat: position.latitude, lng: position.longitude }}
-            style={{
-              width: '90%',
-              height: '120px',
-              borderRadius: '8px',
-              margin: 'auto',
-            }}
+       */}
+      {/* Tab */}
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={tabValue}
+            onChange={handleChange}
+            aria-label="basic tabs example"
           >
-            <MapMarker
-              position={{ lat: position.latitude, lng: position.longitude }}
-            />
-          </Map>
-          <div>{road_address}</div>
-          <button
-            type="button"
-            onClick={copyAdd}
-            className="flex justify-center align-center w-[100px] h-[37px] px-6px pt-16px pb-12px gap-4px rounded-lg border border-solid border-[#E5E7EB]"
-          >
-            <Copy size={14} />
-            주소복사
-          </button>
-        </section>
-      </main>
+            <Tab label="정보" {...a11yProps(0)} />
+            <Tab label="리뷰" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        {/* CustomTabPanel안에 기본적으로 padding이 사방들어감 */}
+        <CustomTabPanel value={tabValue} index={0}>
+          <BottomSheetInfoTab data={data} />
+        </CustomTabPanel>
+        <CustomTabPanel value={tabValue} index={1}>
+          리뷰컴포넌트 넣기
+        </CustomTabPanel>
+      </Box>
     </>
   );
 }
